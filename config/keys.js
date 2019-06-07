0209c6fb1b7db5f7ec1ded6e9952a00632445806
Ranjith Dasthoti <dranjith2012@gmail.com>
2019-06-07 19:38:39 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://Ranjith8:Ranjith8..@practicecluster-2b9h7.mongodb.net/jwt-mern?retryWrites=true&w=majority",
+    "mongodb+srv://<username>:<password>@practicecluster-2b9h7.mongodb.net/jwt-mern?retryWrites=true&w=majority",
   SecretOrKey: "R@nj^t#"
 };
